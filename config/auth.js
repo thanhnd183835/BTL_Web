56b60fba542bd56f610ef479229b9fb20a69d9a3
thanhnd183835 <thanhbk1998@gmail.com>
2022-01-16 21:53:50 +0700

    a

--- config/auth.js
+++ config/auth.js
@@ -1,15 +1,12 @@
 module.exports = {
-    'facebookAuth' : {
-        'clientID'      : '762868437069360', // your App ID
-        'clientSecret'  : '55f4bc8240f580d07d36517faf04913a', // your App Secret
-        'callbackURL'   : 'http://localhost:3000/auth/facebook/callback'
-    },
+  facebookAuth: {
+    clientID: "762868437069360", // your App ID
+    clientSecret: "55f4bc8240f580d07d36517faf04913a", // your App Secret
+  },
 
-    'googleAuth' : {
-        'clientID'      : '11195848805-ibkdjcnu6jbslr3b8tsiiop3dfcn8ed4.apps.googleusercontent.com',
-        'clientSecret'  : '2_pFQkm6bf5OlFWMfkGHqaPd',
-        'callbackURL'   : 'http://localhost:3000/auth/google/callback'
-    }
-  
-
+  googleAuth: {
+    clientID:
+      "11195848805-ibkdjcnu6jbslr3b8tsiiop3dfcn8ed4.apps.googleusercontent.com",
+    clientSecret: "2_pFQkm6bf5OlFWMfkGHqaPd",
+  },
 };
